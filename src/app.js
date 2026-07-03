59aa3de50e98f3c479969b9876bcca1b59bc8817
CuongDoDuc <doduccuong24102005@gmail.com>
2026-07-04 01:13:44 +0700

    Cap nhat file app. Hoan thanh API form va fiel cho Admin

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const formRoutes = require("./routes/form.routes");
+const fieldRoutes = require("./routes/field.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Kiem tra server co hoat dong khong
+// Đăng ký API routes
+app.use("/api/forms", formRoutes);
+app.use("/api/forms", fieldRoutes);
+
 app.get("/ping", (req, res) => {
-  res.status(200).json({ message: ping });
+  res.status(200).json({ message: "pong" });
 });
 
 module.exports = app;
